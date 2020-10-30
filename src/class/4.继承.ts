403c934329aebe7fedf879e73ebe0122aebef589
31536879@qq.com <31536879@qq.com>
2020-10-30 09:21:53 +0800

    面向对象

---
+++ src/class/4.继承.ts
@@ -0,0 +1,27 @@
+// class Person {
+
+//     private _a = 1; 只能在父类中访问，子类中无法访问
+
+//     // 在构造函数的参数中如果直接使用public等修饰符，等同于在构造函数外边声明了该变量
+//     constructor(public username: string, public age: number) {
+//         this.username = username;
+//         this.age = age;
+//     }
+
+// }
+
+// class Student extends Person {
+
+//     // 如果子类没有重写构造函数，则默认子类的构造函就是父类的构造函
+//     // 如果子类重写了构造函数
+//     // 注意：需要手动调用父类构造函数 super()也需要传参
+//     // super：关键字，表示父类
+//     constructor(username: string, age: number, public type: string) {
+//         super(username, age);    //执行父类构造函数
+
+//         this.type = type;
+//     }
+
+// }
+
+// let s1 = new Student('Kimoo', 30, 'javascript');
--- src/class/6-class.ts
+++ src/class/6-class.ts
@@ -1,10 +1,24 @@
 /*与es6的区别  ts中的类，成员属性必须先声明再使用 */
 class person{
-   private username:string = '';
+   private _username:string = ''; // 私有属性一般用_声明
+   private _age:number;
 
-    constructor(name:string)
+    constructor(name:string,age:number)
     {
-        this.username = name;
+        this._username = name;
+        this._age = age
+   
+    }
+
+    public get a() : number {
+        return this._age
+    }
+
+    public set a(age:number) {
+        if(age<200)
+        {
+            this._age = age
+        } 
     }
 }
 
@@ -15,10 +29,13 @@ protected 在类内部和其子类中可访问
 readonly 只读 不能修改
 */
 
-let p1 = new person('wangerniu')
+let p1 = new person('wangerniu',12)
 /* p1.username 无法访问*/
 
+p1.a = 14 // 可以访问person类内部的私有变量，且可以修改（通过get，set存取器），但是是有规则限制的修改，具体规则在set中定义 
+console.log(p1.a); // 打印14 
 
 /* 存取器
 ts支持通过getters和setters来截取对对象成员的访问
 */
+
