751109e9e29b5129897ef6ece0716f6be95b0bd3
郭庆 <31536879@qq.com>
2020-12-31 17:30:33 +0800

    update

--- src/class/4.继承.ts
+++ src/class/4.继承.ts
@@ -13,7 +13,7 @@
 // class Student extends Person {
 
 //     // 如果子类没有重写构造函数，则默认子类的构造函就是父类的构造函
-//     // 如果子类重写了构造函数
+//     // 如果子类重写了构造函数 那么必须super()一下
 //     // 注意：需要手动调用父类构造函数 super()也需要传参
 //     // super：关键字，表示父类
 //     constructor(username: string, age: number, public type: string) {
